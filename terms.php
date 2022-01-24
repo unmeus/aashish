42de12999c5f391d7407dba8aafbfc7f3bb63b83
unmeus <98362541+unmeus@users.noreply.github.com>
2022-01-25 05:09:25 +0545

    Add files via upload

---
+++ terms.php
@@ -0,0 +1,16 @@
+   <!--Modal Log-out --> 
+
+            <div class="modal fade" id="logout" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+
+ <h1>please do not share your password and after clicking this the user is agreeing to be a costumer of our organization and will hence forth have certain rights and responsibilities.</h1>
+
+</div>
+ 
+</div>+</div>  
+                                      
+          <!--Logout end -->  
